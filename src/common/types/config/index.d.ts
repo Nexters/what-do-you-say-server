6c1ed2564fb069f4aead2336920761ee9babd61a
janghyoseog <bestdevhyo1225@gmail.com>
2020-07-15 21:18:06 +0900

    chore: src 및 test 디렉토리 초기화 작업

--- src/common/types/config/index.d.ts
+++ src/common/types/config/index.d.ts
@@ -1,10 +1,12 @@
 import { ConfigAll } from './all'
 import { ConfigTest } from './test'
 import { ConfigDevelopment } from './development'
+import { ConfigProduction } from './production'
 
 export interface ConfigIndex {
   [key: string]: object
   readonly all: ConfigAll
   readonly test: ConfigTest
   readonly development: ConfigDevelopment
+  readonly production: ConfigProduction
 }
---
+++ src/common/types/config/production/index.d.ts
@@ -0,0 +1,11 @@
+import { MysqlConnectionCredentialsOptions } from 'typeorm/driver/mysql/MysqlConnectionCredentialsOptions'
+
+export interface ConfigProduction {
+  readonly db?: {
+    readonly database?: string
+    readonly replication?: {
+      readonly master: MysqlConnectionCredentialsOptions
+      readonly slaves: MysqlConnectionCredentialsOptions[]
+    }
+  }
+}
