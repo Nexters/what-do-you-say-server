3ada6ff361daf6ffcd268c46834df42be4d4a103
janghyoseog <bestdevhyo1225@gmail.com>
2020-07-20 23:33:54 +0900

    feat: 카테고리 엔티티 추가
    [#20]

---
+++ src/entity/Category.ts
@@ -0,0 +1,29 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+
+enum Property {
+  SITUATION = 'situation',
+  HONORIFIC = 'honorific',
+  SENTENCE_LENGTH = 'sentence_length',
+}
+
+@Entity('category')
+@Index(['isDisplay'])
+export class Category extends BaseEntity {
+  @PrimaryGeneratedColumn('increment', { type: 'int' })
+  id!: number
+
+  @Column('enum', { enum: Property, default: Property.SITUATION, comment: '속성' })
+  property!: Property
+
+  @Column('varchar', { default: '', comment: 'ex) 새해, 설날, 어버이날, 존댓말, 반말, 2줄, 3줄 등등...' })
+  value!: string
+
+  @Column('boolean', { default: true, comment: '활성화' })
+  isDisplay!: boolean
+
+  @CreateDateColumn({ name: 'created_at', comment: '등록 날짜' })
+  createdAt!: Date
+
+  @UpdateDateColumn({ name: 'updated_at', comment: '수정 날짜' })
+  updatedAt!: Date
+}
