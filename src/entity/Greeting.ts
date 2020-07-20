c8001740df64ab389c127b6565c4f81628462339
janghyoseog <bestdevhyo1225@gmail.com>
2020-07-20 23:34:20 +0900

    feat: Greeting 엔티티 추가
    [#20]

--- src/entity/Greeting.ts
+++ src/entity/Greeting.ts
@@ -1,11 +1,24 @@
-import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { Bookmark } from './Bookmark'
 
+@Entity('greeting')
+@Index(['situation', 'sentenceLength', 'honorific', 'isDeleted'])
 export class Greeting extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  greetingId!: number
+  @PrimaryGeneratedColumn('increment', { type: 'bigint' })
+  id!: bigint
 
   @Column('varchar', { default: '', comment: '상황' })
-  state!: string
+  situation!: string
 
   @Column('varchar', { default: '', comment: '존대 정도' })
   honorific!: string
@@ -15,4 +28,20 @@ export class Greeting extends BaseEntity {
 
   @Column('text', { default: '', comment: '인사말' })
   contents!: string
+
+  @Column('int', { default: 0, comment: '북마크 카운트' })
+  bookmarkCount!: number
+
+  @Column('boolean', { default: false, comment: '삭제 여부' })
+  isDeleted!: boolean
+
+  @CreateDateColumn({ name: 'created_at', comment: '등록 날짜' })
+  createdAt!: Date
+
+  @UpdateDateColumn({ name: 'updated_at', comment: '수정 날짜' })
+  updatedAt!: Date
+
+  @ManyToMany(() => Bookmark, { cascade: true })
+  @JoinTable({ name: 'greeting_bookmark' })
+  bookmarks!: Bookmark[]
 }
