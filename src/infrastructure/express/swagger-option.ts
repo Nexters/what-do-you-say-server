f7ef1d477d6f1b6bbf18005e0202aa9981c2000e
janghyoseog <bestdevhyo1225@gmail.com>
2020-08-02 04:42:00 +0900

    feat: swagger 미들웨어 추가

---
+++ src/infrastructure/express/swagger-option.ts
@@ -0,0 +1,41 @@
+import path from 'path'
+
+export default {
+  swaggerDefinition: {
+    info: {
+      title: 'What do you say API TEST',
+      version: '1.0.0',
+      description: 'API Test',
+    },
+    basePath: '/',
+    components: {
+      res: {
+        BadRequest: {
+          description: '잘못된 요청입니다.',
+          schema: {
+            $ref: '#/components/errorResult/Error',
+          },
+        },
+        NotFound: {
+          description: '존재하지 않는 리소스를 요청했습니다.',
+          schema: {
+            $ref: '#/components/errorResult/Error',
+          },
+        },
+      },
+      errorResult: {
+        Error: {
+          type: 'object',
+          properties: {
+            errMsg: {
+              type: 'string',
+              description: '에러 메시지 전달',
+            },
+          },
+        },
+      },
+    },
+    schemes: ['http', 'https'],
+  },
+  apis: [path.join(__dirname, '../../controller/*Controller.{js,ts}')],
+}
