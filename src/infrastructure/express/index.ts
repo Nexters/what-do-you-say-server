81425ee9f7875dfb7fd54695fe5e97ec39ddeb17
janghyoseog <bestdevhyo1225@gmail.com>
2020-08-02 13:20:16 +0900

    feat: swagger pet-store 추가

--- src/infrastructure/express/index.ts
+++ src/infrastructure/express/index.ts
@@ -7,7 +7,8 @@ import swaggerUI from 'swagger-ui-express'
 import jsend from 'jsend'
 import cors from 'cors'
 
-import swaggerOption from '@infrastructure/express/swagger-option'
+import swaggerPetStoreOption from '@infrastructure/express/swagger-petstore-option'
+import swaggerApiOption from '@infrastructure/express/swagger-api-option'
 import config from '@common/config'
 
 export default (container: AwilixContainer): Express => {
@@ -16,7 +17,8 @@ export default (container: AwilixContainer): Express => {
   const app: Express = express()
   const httpLogging: Debugger = debug('http')
   const appServerLogging: Debugger = debug('app:server')
-  const swaggerSpec: object = swaggerJSDoc(swaggerOption)
+  const swaggerApiSpec: object = swaggerJSDoc(swaggerApiOption)
+  const swaggerPetStoreSpec: object = swaggerJSDoc(swaggerPetStoreOption)
 
   app.use(cors())
   app.use((req: Request, res: Response, next: NextFunction) => {
@@ -30,9 +32,27 @@ export default (container: AwilixContainer): Express => {
 
   app.use(scopePerRequest(container))
   app.use(loadControllers('../../controller/*Controller.{js,ts}', { cwd: __dirname }))
-  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerApiSpec))
+  app.use('/swagger-json', swaggerUI.serve, swaggerUI.setup(swaggerPetStoreSpec))
 
-  app.get('/health-check', (req: Request, res: Response) => res.status(200).json({ healthy: true }))
+  /**
+   * @swagger
+   * tags:
+   *  name: Health Check
+   *  description: Server 상태를 확인하는 API
+   */
+
+  /**
+   *  @swagger
+   *  /health-check:
+   *    get:
+   *      summray: Server의 상태가 살아있는지 확인한다.
+   *      tags: [Health Check]
+   *      responses:
+   *        200:
+   *          description: 성공
+   */
+  app.use('/health-check', (req: Request, res: Response) => res.status(200).json({ healthy: true }))
 
   app.listen(port, host, () => {
     appServerLogging('Express server listening on %s:%d, in %s mode', host, port, env)
