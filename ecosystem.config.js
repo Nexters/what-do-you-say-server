3360db35d05d7c16750cbb5b2e8d4838c67796bd
janghyoseog <bestdevhyo1225@gmail.com>
2020-08-05 21:19:22 +0900

    chore: Production 환경에서 무중단 배포를 위한 pm2 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'what-do-you-say',
+      script: 'dist/.',
+      instances: 0,
+      exec_mode: 'cluster',
+      wait_ready: true,
+      listen_timeout: 50000,
+      kill_timeout: 5000,
+    },
+  ],
+}
