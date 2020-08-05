f3da198a81974bbfccf0973be51d858ae44949e0
janghyoseog <bestdevhyo1225@gmail.com>
2020-08-05 23:56:06 +0900

    refactor: 무중단 배포 시스템 변경

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-DOCKER_APP_NAME=what-do-you-say-server
-
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
-
-if [ -z "$EXIST_BLUE" ]; then
-  echo "Blue Server Start"
-
-  docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
-
-  sleep 10
-
-  docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-else
-  echo "Green Server Start"
-
-  docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
-
-  sleep 10
-
-  docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-fi
+npm run bulid && npm run prod
