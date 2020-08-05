64ce95166aafb2a5fd53318fae273cdb36a0ce1f
janghyoseog <bestdevhyo1225@gmail.com>
2020-08-06 01:55:12 +0900

    feat: 배포 스크립트 재 수정

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo npm run prod
+DOCKER_APP_NAME=what-do-you-say-server
+
+EXIST_SERVER=$(docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.server.yml ps | grep Up)
+
+if [ -z "$EXIST_SERVER" ]; then
+  echo "Server start in docker"
+
+  docker-compose -f docker-compose.server.yml up
+else
+  echo "Server reload in docker"
+
+  docker exec what-do-you-say-server pm2 reload ecosystem.config.js
+fi
