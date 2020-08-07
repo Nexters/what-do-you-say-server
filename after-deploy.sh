b034dcb6f0eac58dceba93e5cc09f85d029ce2f6
janghyoseog <bestdevhyo1225@gmail.com>
2020-08-07 20:07:00 +0900

    chore: 환경 변수를 저장하도록 스크립트 구성2

--- after-deploy.sh
+++ after-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/ubuntu/deploy/what-do-you-say-server &&
-sudo cp ../server-environment/.env ./.env &&
-./start-docker-container.sh > /dev/null 2> /dev/null < /dev/null &
+sudo cp ../server-environment/.env ./.env
+#./start-docker-container.sh > /dev/null 2> /dev/null < /dev/null &
